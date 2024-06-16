5a57bab3d38ce717ed143d65f587d970c078c207
onexeuo <168074915+onexeuo@users.noreply.github.com>
2024-06-16 17:43:00 +0900

    gg

--- HTML/memo/js/memo.js
+++ HTML/memo/js/memo.js
@@ -29,33 +29,37 @@ $(function(){
 
     printMemoList();
 
-    function printMemoList(){
-        $("#list").append(getTopMemo);
-    }
-
     //제목과 내용을 입력하고 등록 버튼 누르면 메모 객체 생성해서 addMemo 호출
     $("#addBtn").click(function(){
         const memoObj = {
             title : $("#addTitle").val(),
-            content:  $("#addTitle").val(),
-            regdate: Date.now()
+            content:  $("#addContent").val(),
+            regdate: newDate(Date.now())
         };
-
         addMemo(memoObj);
     });
 
-    $("#deleteBtn").click(function(){
+    //delete 를 눌렀을때
+    $(document).on('click','.deleteBtn',function(){
+        const memoId = $(this).parent().attr("id").substr(4);
+        removeMemo(memoId);
+    })
 
-    });
 
+    //리스트를 클릭했을때 오른쪽에 내용이 보일 수 있게 하기
     $("#list li").click(function(){
-        console.log($(this).attr("id").substr(4));
+        const memoId = $(this).attr("id").substr(4);
+        const memoList = getMemoList();
+        const memo = memoList[memoId];
+        $("#h1Text").text(memo.regdate);
+        $("#h2Text").text(memo.title);
+        $("#h3Text").text(memo.content);
     })
 
 
     //날짜
     function newDate(regdate){
-        return moment(regdate,"YY/MM/DD HH:mm").format("YY/MM/DD HH:mm");
+        return moment(regdate).format("YY/MM/DD HH:mm");
     }
 
     //localStorage의 메모리스트를 가져오는 함수
@@ -79,18 +83,30 @@ $(function(){
     }
 
     //localStorage의 메모리스트에서 메모를 삭제하는 함수
+    //splice() : 배열의 요소를 삭제하는 메소드 ,리스트배열에 인덱스 1을 제거
     function removeMemo(key){
-        const memoIdx = memoList[key]
-
+        const memoListArr = getMemoList(); 
+        memoListArr.splice(key,1);
+        localStorage.setItem("memoList", JSON.stringify(memoListArr));
+        printMemoList();
     }
 
     //최신메모를 content에 보여주는 함수
-    function getTopMemo(){
+    function printMemoList(){
         $("#list").empty();
         const memoList = getMemoList().reverse();
         const memoListLeng = memoList.length;
         for(let i=0; i<memoListLeng; i++){
             $("#list").append("<li id='memo"+i+"'><br />"+ memoList[i].title+"<input class='deleteBtn' type='button' value='delete'/></li>");
+        }if (memoListLeng > 0){
+            const firstMemo = memoList[memoListLeng -1 ];
+            $("#h1Text").text(firstMemo.regdate);
+            $("#h2Text").text(firstMemo.title);
+            $("#h3Text").text(firstMemo.content);
+        }else{
+            $("#h1Text").empty();
+            $("#h2Text").empty();
+            $("#h3Text").empty();
         }
     }
 
